5eb5960ede680738ec24ccb54fdddee0f0f1f5de
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-02-27 17:33:42 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$MainMenu = {
+$Menu = {
    Write-Host " ******************************************************"
    Write-Host " * Microsoft Office Installation Script               *" 
    Write-Host " * Date:    26/02/2023                                *" 
@@ -6,28 +6,62 @@ $MainMenu = {
    Write-Host " * Website: https://bonben365.com/                    *" 
    Write-Host " ******************************************************" 
    Write-Host 
-   Write-Host " 1. Office 365 / Microsoft 365" 
-   Write-Host " 2. Uninstall All Previous Versions of Microsoft Office"
-   Write-Host " 3. Quit or Press Ctrl + C"
+   Write-Host " 1. Install Office 365 / Microsoft 365 (32bit)" 
+   Write-Host " 2. Install Office 365 / Microsoft 365 (64bit)" 
+   Write-Host " 3. Convert Office 365 / Microsoft 365 from 32-bit to 64-bit" 
+   Write-Host " 4. Convert Office 365 / Microsoft 365 from 64-bit to 32-bit" 
+   Write-Host " 5. Uninstall All Previous Versions of Microsoft Office"
+   Write-Host " 6. Quit or Press Ctrl + C"
    Write-Host 
    Write-Host " Select an option and press Enter: "  -nonewline
    }
    cls
+
+$install = {
+   $null = New-Item -Path $env:temp\c2r -ItemType Directory -Force
+   Set-Location $env:temp\c2r
+   $fileName = "configuration-x$arch.xml"
+   New-Item $fileName -ItemType File -Force | Out-Null
+   Add-Content $fileName -Value '<Configuration>'
+   Add-content $fileName -Value '<Add OfficeClientEdition="64" Channel="Current">'
+   Add-content $fileName -Value "<Product ID=`"$productId`">"
+   Add-content $fileName -Value '<Language ID="en-us" />'
+   Add-Content $fileName -Value '</Product>'
+   Add-Content $fileName -Value '</Add>'
+   Add-Content $fileName -Value '</Configuration>'
+   Write-Host
+   Write-Host ============================================================
+   Write-Host "Installing $productId $arch bit"
+   Write-Host ============================================================
+   Write-Host
+
+   $uri = 'https://github.com/bonben365/office365-installer/raw/main/setup.exe'
+   $null = Invoke-WebRequest -Uri $uri -OutFile 'setup.exe' -ErrorAction:SilentlyContinue
+   .\setup.exe /configure .\$fileName
+   
+   # Cleanup
+   Set-Location "$env:temp"
+   Remove-Item $env:temp\c2r -Recurse -Force
+}
    
    Do { 
-   cls
-   Invoke-Command $MainMenu
-   $Select = Read-Host
+      cls
+      Invoke-Command $Menu
+      $Select = Read-Host
+
+      if ($select -eq 1) {$arch = '32'}
+      if ($select -eq 1) {$arch = '64'}
+
    Switch ($Select)
       {
          #1. Office 365 / Microsoft 365
           1 {
-               $365SubMenu = {
+               $Menu1 = {
                Write-Host " *************************************************"
                Write-Host " * Select a Microsoft Office 365 Product         *" 
                Write-Host " *************************************************" 
                Write-Host 
-               Write-Host " 1. Office 365 Home" 
+               Write-Host " 1. Office 365 Home"
                Write-Host " 2. Office 365 Personal"
                Write-Host " 3. Microsoft 365 Apps for Business" 
                Write-Host " 4. Microsoft 365 Apps for Enterprise" 
@@ -38,54 +72,25 @@ $MainMenu = {
                cls
                         
             Do { 
-            cls
-            Invoke-Command $365SubMenu
-            $365SubSelect = Read-Host
+               cls
+               Invoke-Command $Menu1
+               $Select1 = Read-Host
    
-            if ($365SubSelect -eq 1) {$productId = 'O365HomePremRetail'}
-            if ($365SubSelect -eq 2) {$productId = 'O365HomePremRetail'}
-            if ($365SubSelect -eq 3) {$productId = 'O365BusinessRetail'}
-            if ($365SubSelect -eq 4) {$productId = 'O365ProPlusRetail'}
-   
-            $o365 = {
-               $null = New-Item -Path $env:temp\c2r -ItemType Directory -Force
-               Set-Location $env:temp\c2r
-               $fileName = 'configuration.xml'
-               New-Item $fileName -ItemType File -Force | Out-Null
-               Add-Content $fileName '<Configuration>'
-               Add-content $fileName '<Add OfficeClientEdition="64" Channel="Current">'
-               Add-content $fileName "<Product ID=`"$productId`">"
-               Add-content $fileName '<Language ID="en-us" />'
-               Add-Content $fileName -Value '</Product>'
-               Add-Content $fileName -Value '</Add>'
-               Add-Content $fileName -Value '</Configuration>'
-               Write-Host
-               Write-Host ============================================================
-               Write-Host "Installing $productId...."
-               Write-Host ============================================================
-               Write-Host
-
-               $uri = 'https://github.com/bonben365/office365-installer/raw/main/setup.exe'
-               Invoke-WebRequest -Uri $uri -OutFile 'setup.exe' -ErrorAction:SilentlyContinue | Out-Null
-               .\setup.exe /configure .\configuration.xml
-               
-               # Cleanup
-               Set-Location "$env:temp"
-               Remove-Item $env:temp\c2r -Recurse -Force
-           }
+               if ($Select1 -eq 1) {$productId = 'O365HomePremRetail'}
+               if ($Select1 -eq 2) {$productId = 'O365HomePremRetail'}
+               if ($Select1 -eq 3) {$productId = 'O365BusinessRetail'}
+               if ($Select1 -eq 4) {$productId = 'O365ProPlusRetail'}
        
-           Switch ($365SubSelect)
+           Switch ($Select1)
            {
-                1 { Invoke-Command $o365 }
-                2 { Invoke-Command $o365 }
-                3 { Invoke-Command $o365 }
-                4 { Invoke-Command $o365 }
+                1 { Invoke-Command $install }
+                2 { Invoke-Command $install }
         
             }
                }
-            While ($365SubSelect -ne 5)
+            While ($Select1 -ne 5)
             cls
             }  
       }
    }
-   While ($Select -ne 3)
+   While ($Select -ne 6)
